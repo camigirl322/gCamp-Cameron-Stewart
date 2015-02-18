f93c14f501e3c80bd52a26c054f0e1312749bd49
Cameron Webster <cameron.webster@gmail.com>
2015-02-18 13:42:27 -0700

    user must enter first name, last name, email on edit of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,8 +33,16 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to users_url, notice: 'User was successfully updated'
+
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to @user, notice: 'User was successfully updated.' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
   end
 
   def destroy
