0e1455d6bcc3e722e41dd03f87a762b7ccc93ef3
Cameron Webster <cameron.webster@gmail.com>
2015-02-26 11:48:06 -0700

    add project CRUD spec

---
+++ spec/features/projects_crud_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe "user can CRUD projects" do
+  before :each, 'user logs in' do
+    @user = User.create(first_name: "Cameron", last_name: "Stew", email: "cam@awesome.com", password: "awesome", password_confirmation: "awesome")
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_button 'Sign In'
+    expect(page).to have_content "#{@user.first_name} #{@user.last_name}"
+    click_link 'Projects'
+  end
+
+  it 'User can see project index page' do
+    expect(page).to have_content 'Projects'
+  end
+
+  it "User can create project and view show page" do
+      click_link 'New Project'
+      expect(page).to have_content 'Create Project'
+      fill_in 'Name', with: 'Awesome Project!'
+      click_button 'Create Project'
+      expect(page).to have_content 'Project was successfully created.'
+  end
+
+  it 'User can edit existing project' do
+    @project = Project.create(name: 'Sweet Project')
+    visit("/projects/#{@project.id}/edit")
+    expect(page).to have_content 'Edit Project'
+    fill_in 'Name', with: 'Sweeter Project'
+    click_button 'Update Project'
+    expect(page).to have_content 'Project was successfully updated.'
+  end
+
+  it "User can delete existing project" do
+    @project = Project.create(name: 'Sweet Project')
+    visit("/projects/#{@project.id}/edit")
+    click_link 'Delete Project'
+    expect(page).to have_content 'Project was successfully deleted'
+  end
+end
