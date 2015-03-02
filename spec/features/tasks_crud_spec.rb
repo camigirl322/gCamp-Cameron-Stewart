ded6e1e16f6b114e377bf18f690f422eb70e0271
Cameron Webster <cameron.webster@gmail.com>
2015-03-02 13:32:38 -0700

    update tasks capybara

--- spec/features/tasks_crud_spec.rb
+++ spec/features/tasks_crud_spec.rb
@@ -3,13 +3,18 @@ require 'rails_helper'
 describe 'User can CRUD tasks' do
 
   before :each do
+    @user = User.create(first_name: "Cameron", last_name: "Stew", email: "cam@awesome.com", password: "awesome", password_confirmation: "awesome")
     visit root_path
-    click_link 'Tasks'
+    click_link 'Sign In'
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_button 'Sign In'
+
+    @project = Project.create(name: 'Sweet Project')
+    visit("/projects/#{@project.id}/tasks")
+    expect(page).to have_content "No tasks for this project yet! "
   end
 
-  it 'User can see tasks page' do
-    expect(page).to have_content 'Tasks'
-  end
 
   it 'User can create task' do
     click_link 'New Task'
@@ -21,13 +26,13 @@ describe 'User can CRUD tasks' do
 
   it 'User can view task show page' do
     @task = Task.create(description: 'Task 2', due_date: '2/16/14', complete: false)
-    visit("/tasks/#{@task.id}")
+    visit("/projects/#{@project.id}/tasks/#{@task.id}")
     expect(page).to have_content @task.description
   end
 
   it 'User can edit task' do
     @task = Task.create(description: 'Task 2', due_date: '2/16/14', complete: false)
-    visit("/tasks/#{@task.id}")
+    visit("/projects/#{@project.id}/tasks/#{@task.id}")
     expect(page).to have_content @task.description
     click_link 'Edit'
     fill_in 'Description', with: 'Task 2 Update'
@@ -37,7 +42,7 @@ describe 'User can CRUD tasks' do
 
   it 'User can delete task' do
     @task = Task.create(description: 'Task 2', due_date: '2/16/14', complete: false)
-    visit("/tasks/#{@task.id}")
+    visit("/projects/#{@project.id}/tasks/#{@task.id}")
     expect(page).to have_content @task.description
     click_link 'Edit'
     click_link 'Delete Task'
