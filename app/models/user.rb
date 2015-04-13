c23a6320b45ab0b12cce4f7d0e410017a5cc24c6
Cameron Webster <cameron.webster@gmail.com>
2015-04-13 12:07:17 -0600

    add dependent destroy to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :memberships
+  has_many :comments, dependent: :destroy
+  has_many :memberships, dependent: :destroy
   has_many :projects, through: :memberships
+  has_many :tasks, through: :comments
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
