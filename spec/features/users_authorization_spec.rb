a2984d9cd5d557c36985b7087706cb9b352d378d
Cameron Webster <cameron.webster@gmail.com>
2015-04-15 20:35:47 -0600

    fix user authorization spec

--- spec/features/users_authorization_spec.rb
+++ spec/features/users_authorization_spec.rb
@@ -9,12 +9,16 @@ describe 'user must be logged in to view user page' do
     fill_in 'Password', with: @user.password
     click_button 'Sign In'
     expect(page).to have_content "#{@user.first_name} #{@user.last_name}"
-    click_link 'Users'
+    within('.col-md-4') do
+      click_link 'Users'
+    end
   end
 
   it 'logged in users can access user page' do
     visit("/users")
-    expect(page).to have_content "Users"
+    within('h1') do
+      expect(page).to have_content "Users"
+    end
   end
   it 'user cant access users until logged in' do
     click_link 'Sign Out'
